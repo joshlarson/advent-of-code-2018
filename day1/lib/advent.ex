bf8dff07c30c71bbf52a0b1fea069cd03a73ca15
Josh Larson <joshua.larson@getbraintree.com>
2018-12-16 15:09:48 -0600

    Day 1, Part 2

--- day1/lib/advent.ex
+++ day1/lib/advent.ex
@@ -7,8 +7,36 @@ defmodule Advent do
     content
     |> final_frequency
     |> IO.puts
+
+    content
+    |> first_aggregate_duplicate
+    |> IO.puts
   end
-  
+
+  def first_aggregate_duplicate(input) do
+    aggregate_frequencies(input)
+    |> first_duplicate
+  end
+
+  def aggregate_frequencies(input) do
+    frequency_deltas(input)
+    |> Stream.cycle
+    |> Stream.scan(&+/2)
+  end
+
+  def first_duplicate(list) do
+    list
+    |> Enum.reduce_while(MapSet.new([0]), &check_for_duplicate/2)
+  end
+
+  defp check_for_duplicate(x, set) do
+    if x in set do
+      {:halt, x}
+    else
+      {:cont, set |> MapSet.put(x) }
+    end
+  end
+
   def final_frequency(input) do
     frequency_deltas(input)
     |> Enum.sum
@@ -18,6 +46,7 @@ defmodule Advent do
     input
     |> String.split("\n")
     |> Enum.map(&parse_directly/1)
+    |> Enum.filter(&(&1 != 0))
   end
 
   defp parse_directly(str) do
