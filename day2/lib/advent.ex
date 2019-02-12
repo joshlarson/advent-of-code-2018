0cd0d24ca30a8f5e8561a6f9767cb3bda247c65e
Josh Larson <joshua.larson@getbraintree.com>
2019-02-12 12:58:20 -0600

    Day 2, Part 2

--- day2/lib/advent.ex
+++ day2/lib/advent.ex
@@ -7,6 +7,10 @@ defmodule Advent do
     content
     |> checksum
     |> IO.puts
+
+    content
+    |> common_letters
+    |> IO.puts
   end
 
   def checksum(input) do
@@ -24,5 +28,31 @@ defmodule Advent do
     |> String.codepoints
     |> Enum.group_by(&(&1))
     |> Enum.any?(fn {_, value} -> Enum.count(value) == n end)
+  end
+
+  def common_letters(input) do
+    box_ids = String.split(input, "\n")
+              |> Enum.map(&String.codepoints/1)
+
+    {id1, id2} = box_ids
+                 |> Enum.map(fn id -> {id, find_with_one_mismatch(id, box_ids)} end)
+                 |> Enum.filter(fn {_, other_ids} -> other_ids |> Enum.any? end)
+                 |> Enum.map(fn {id, other_ids} -> {id, List.first(other_ids)} end)
+                 |> List.first
+
+    Enum.zip(id1, id2)
+    |> Enum.filter(fn {x, y} -> x == y end)
+    |> Enum.map(fn {x, _} -> x end)
+    |> Enum.join
+  end
+
+  defp find_with_one_mismatch(id1, box_ids) do
+    box_ids
+    |> Enum.filter(fn other_id -> count_mismatches(id1, other_id) == 1 end)
+  end
+
+  def count_mismatches(id1, id2) do
+    Enum.zip(id1, id2)
+    |> Enum.count(fn {x, y} -> x != y end)
   end
 end
--- day2/test/advent_test.exs
+++ day2/test/advent_test.exs
@@ -1,27 +1,53 @@
 defmodule AdventTest do
   use ExUnit.Case
 
-  test "for 1 input, checksum is 1 if there are letters that happen 2 and 3 times" do
-    assert Advent.checksum("aabbb\n") == 1
+  describe "checksum" do
+    test "for 1 input, checksum is 1 if there are letters that happen 2 and 3 times" do
+      assert Advent.checksum("aabbb\n") == 1
+    end
+
+    test "for 1 input, checksum is 0 if there are no letters that happen 3 times" do
+      assert Advent.checksum("aabb\n") == 0
+    end
+
+    test "for 1 input, checksum is 0 if there are no letters that happen 2 times" do
+      assert Advent.checksum("aaabbb\n") == 0
+    end
+
+    test "for 2 inputs, checksum is 1 if one string matches 2 and the other matches 3" do
+      assert Advent.checksum("aabb\naaabbb\n") == 1
+    end
+
+    test "for 2 inputs, checksum is 1 if one string matches both and the other matches nothing" do
+      assert Advent.checksum("aabbb\nabbbb\n") == 1
+    end
+
+    test "for 2 inputs, checksum is 4 if both strings match both" do
+      assert Advent.checksum("aabbb\naaabb\n") == 4
+    end
   end
 
-  test "for 1 input, checksum is 0 if there are no letters that happen 3 times" do
-    assert Advent.checksum("aabb\n") == 0
+  describe "common_letters" do
+    test "finds the common letters if there are two strings" do
+      assert Advent.common_letters("a\nb\n") == ""
+      assert Advent.common_letters("ac\nab\n") == "a"
+      assert Advent.common_letters("ca\nba\n") == "a"
+      assert Advent.common_letters("ecb\nebb\n") == "eb"
+    end
+
+    test "finds the commons letters between the right pair of strings" do
+      assert Advent.common_letters("abc\nabb\nxyz\n") == "ab"
+      assert Advent.common_letters("abc\nxyz\nabb\n") == "ab"
+      assert Advent.common_letters("xyz\nabc\nabb\n") == "ab"
+    end
   end
 
-  test "for 1 input, checksum is 0 if there are no letters that happen 2 times" do
-    assert Advent.checksum("aaabbb\n") == 0
-  end
-
-  test "for 2 inputs, checksum is 1 if one string matches 2 and the other matches 3" do
-    assert Advent.checksum("aabb\naaabbb\n") == 1
-  end
-
-  test "for 2 inputs, checksum is 1 if one string matches both and the other matches nothing" do
-    assert Advent.checksum("aabbb\nabbbb\n") == 1
-  end
-
-  test "for 2 inputs, checksum is 4 if both strings match both" do
-    assert Advent.checksum("aabbb\naaabb\n") == 4
+  describe "count_mismatches" do
+    test "finds the number of common letters between two strings" do
+      assert Advent.count_mismatches([], []) == 0
+      assert Advent.count_mismatches(["a"], ["a"]) == 0
+      assert Advent.count_mismatches(["a"], ["b"]) == 1
+      assert Advent.count_mismatches(["a", "b", "c", "d"], ["a", "a", "a", "a"]) == 3
+    end
   end
 end
